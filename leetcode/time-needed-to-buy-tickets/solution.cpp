4d226e24a4993be9483a3c5d091f816cfa12d64c
Himanshu <hgautam2010@gmail.com>
2026-07-06 10:53:33 +0000

    preptin: export Time Needed To Buy Tickets: solution

---
+++ leetcode/time-needed-to-buy-tickets/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int result = 0;
+        for (int i = 0; i < tickets.size(); i++) {
+            if (i <= k) {
+                result += min(tickets[k], tickets[i]);
+            } else {
+                result += min(tickets[k] - 1, tickets[i]);
+            }
+        }
+        return result;
+    }
+};
