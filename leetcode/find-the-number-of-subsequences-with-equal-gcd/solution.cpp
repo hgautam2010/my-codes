89305f4e5c63c56c212d51f7d5f1b0d06e7cd899
Himanshu <hgautam2010@gmail.com>
2026-07-15 11:54:23 +0000

    preptin: export 3336. Find the Number of Subsequences With Equal GCD: solution

---
+++ leetcode/find-the-number-of-subsequences-with-equal-gcd/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+
+    int solve(vector<vector<vector<int>>>& dp, vector<int>& nums, int i, int first, int second) {
+        if (i == nums.size()) {
+            return (first == second && first != 0 && second != 0);
+        }
+
+        if (dp[i][first][second] != -1) {
+            return dp[i][first][second];
+        }
+
+        int skip = solve(dp, nums, i + 1, first, second);
+        int firstCo = solve(dp, nums, i + 1, __gcd(first, nums[i]), second);
+        int secondCo = solve(dp, nums, i + 1, first, __gcd(second, nums[i]));
+
+        dp[i][first][second] = (0ll + skip + firstCo + secondCo) % MOD;
+
+        return dp[i][first][second];
+    }
+
+    int subsequencePairCount(vector<int>& nums) {
+        vector<vector<vector<int>>> dp(nums.size(), vector(201, vector(201, -1)));
+        return solve(dp, nums, 0, 0, 0);
+    }
+};
